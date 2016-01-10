34be9faa6225ea1d24760e220522691218fc6ced
Liam Wyatt <Liam Wyatt>
2016-01-10 13:38:01 -0800

    Creation of "GUI" and basic links

--- index.php
+++ index.php
@@ -6,19 +6,18 @@
  * and open the template in the editor.
  */
 
-
+$test = new Game('---------');
+$test->display();
 
 
 if (isset($_GET['board'])) {
     
     $position = $_GET['board'];
-    $squares = str_split($position);
+    $game = new Game($position);
     
-    if (winner('x', $squares)) echo 'You win.';
-    else if (winner('o', $squares)) echo 'I win';
-    else echo 'No winner yet';
-} else {
-    echo 'No board found';
+    if ($game->winner('x')) echo 'You win. Lucky guesses!';
+    else if ($game->winner('o')) echo 'I win. Muahahah';
+    else echo 'No winner yet, but you are losing.';
 }
 
 class Game {
@@ -27,23 +26,51 @@ class Game {
     function __construct($squares) {
         $this->position = str_split($squares);
     }
-    function winner ($token, $position) {
+    function display() {
+        echo "Welcome to the ultimate TicTacToe Experience. </br></br>";
+        
+        echo '<table cols="3" style="font-size:large; font-weight:bold">';
+        echo '<tr>'; //opens the first row of the table
+        for ($pos=0; $pos<9; $pos++) {
+            echo '<td>';
+            echo $this->show_cell($pos);
+            echo '</td>';
+            if ($pos %3 == 2) echo '</tr><tr>'; //new row after 3 cells
+        }
+            echo '</tr>';
+            echo '</table>';
+        
+    }
+    
+    function show_cell($cell) {
+        $token = $this->position[$cell];
+        //easy case
+        if ($token <> '-') return $token;
+        //hard case
+        $this->newposition = $this->position; //copy of original
+        $this->newposition[$token] = 'o'; //their move
+        $move = implode($this->newposition); //make string from array
+        $link = '/?board='.$move;
+        return '<a href="'.$link.'">-</a>';
+    }
+    
+    function winner ($token) {
     $result = false;
     for ($row=0; $row<3; $row++) {
-            if (($position[3*$row] == $token) && ($position[3*$row+1] == $token)
-                        && ($position[3*$row+2] == $token)) $result = true;
+            if (($this->position[3*$row] == $token) && ($this->position[3*$row+1] == $token)
+                        && ($this->position[3*$row+2] == $token)) $result = true;
     }
     for ($col=0; $col<3; $col++) {
-            if (($position[$col] == $token) && ($position[$col+3] == $token)
-                        && ($position[$col+6] == $token)) $result = true;
+            if (($this->position[$col] == $token) && ($this->position[$col+3] == $token)
+                        && ($this->position[$col+6] == $token)) $result = true;
     }
-    if (($position[0] == $token) &&
-        ($position[4] == $token) &&
-        ($position[8] == $token)) {
+    if (($this->position[0] == $token) &&
+        ($this->position[4] == $token) &&
+        ($this->position[8] == $token)) {
     $result = true;
-    }else if (($position[6] == $token) &&
-        ($position[4] == $token) &&
-        ($position[6] == $token)) {
+    }else if (($this->position[6] == $token) &&
+        ($this->position[4] == $token) &&
+        ($this->position[6] == $token)) {
     $result = true;
     }
     return $result;
